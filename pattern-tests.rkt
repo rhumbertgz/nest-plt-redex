dc004cc9406c634e6bc351300b3008844465d92f
Humberto Rodríguez A <rhumbertgz@users.noreply.github.com>
2021-04-15 21:01:10 +0200

    Remove unused imports

--- pattern-tests.rkt
+++ pattern-tests.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require redex  "nest-reductions.rkt" "nest-utils.rkt" "nest-syntax.rkt" "random-pattern-tests.rkt")
+(require redex  "nest-reductions.rkt" "pattern-tests-suite.rkt")
 
 (pattern-test  NEST-T-Reductions            
                ;#:pattern (term (pattern p5 ((and (:msg1 a b) (or ((:msg2 b c) (count 2)) ((:msg3 c d) (every 6)))) (when (> a c)))))
@@ -8,10 +8,11 @@
                ;#:pattern (term (pattern p5 (and ((:msg1 a b) (count 2)) ((:msg2 c d) (every 6)))))
                ;#:pattern (term (pattern p5 (and ((:msg1 a b) (count 2)) (:msg2 c d))))
                ;#:pattern (term (pattern p5 ((and (:msg1 a b) (:msg2 c b)) (when (> a c)))))
-               ;#:pattern (term (pattern p5 (and (:msg1 a b) (:msg2 a d))))
+               ;#:pattern (term (pattern p5 (andThen (:msg1 a b) (:msg2 a d))))
+               #:pattern (term (pattern p5 (and (:msg1 a b) (:msg2 a d))))
                ;#:pattern (term (pattern p1 (((:msgq x y) (count 2)) (when (> x y)) ))) 
               ; #:pattern (term (pattern p1 ((:msgq x y) (every 7)))) 
-               #:pattern (term (pattern p1 ((:msgq x y) (window 10 mins)))) 
+               ;#:pattern (term (pattern p1 ((:msgq x y) (window 10 mins)))) 
                ;#:pattern (term (pattern p1 ((:msgq x y) (count 7)))) 
                ;#:pattern (term (pattern p1 ((:msgq x y) (when (< x y)))))    
                ;#:pattern (term (pattern p1 ((:msgq x y) (when (> x y))))) 
